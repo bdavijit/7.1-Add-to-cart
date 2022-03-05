e1d1ca39433e6ec0419d920866053b7061874c9c
bdavijit <bdavijit@gmail.com>
2022-03-05 14:32:24 +0600

    update notification number

--- js/add_To_cart.js
+++ js/add_To_cart.js
@@ -1,36 +1,42 @@
 let cart_Item_num = 0;
 
-const loadCard = () =>{
-   location.href = "/cart.html";
-}
-const AddToCart = (id) =>{
+const loadCard = () => {
+  location.href = "/cart.html";
+};
+const AddToCart = (id) => {
   let cartData = GetCartData();
-  console.log(cartData);
-  
-  if(cartData.length === 0){
-   cartData = [{id: id , quantity:1}];
-  }else{
-   cartData = [...cartData, {id: id , quantity:1}];
+
+  storeData(cartData, id);
+
+  upDate_Notification();
+};
+const upDate_Notification = () => {
+  let NotificationNumber = localStorage.getItem("cart_Item_num");
+  document.getElementById("cart_Item_num").innerText = JSON.parse(NotificationNumber);
+};
+
+const storeData = (cartData, id) => {
+  console.log("ok");
+  if (cartData.length === 0) {
+    cartData = [{ id: id, quantity: 1 }];
+  } else {
+    cartData = [...cartData, { id: id, quantity: 1 }];
   }
-  
-  localStorage.setItem('cart' , JSON.stringify(cartData));
-  localStorage.setItem('cart_Item_num' , JSON.stringify(cartData.length));
 
-  cart_Item_num = cartData.length;
-  document.getElementById('cart_Item_num').innerText = cart_Item_num;
-}
+  localStorage.setItem("cart", JSON.stringify(cartData));
+  localStorage.setItem("cart_Item_num", JSON.stringify(cartData.length));
 
+  //   cart_Item_num = cartData.length;
+};
 
-const GetCartData = (id) =>{
-  const cartData = localStorage.getItem('cart');
+const GetCartData = () => {
+  const cartData = localStorage.getItem("cart");
 
-if (cartData === null){
-   return [];
-}else{
-   return JSON.parse(cartData); 
-}
-}
+  if (cartData === null) {
+    return [];
+  } else {
+    return JSON.parse(cartData);
+  }
+};
 
-
-// let cartData = [{id:1 , num:1}];
-// localStorage.setItem('cart', JSON.stringify(cartData));
+upDate_Notification();
